f3051b53d87c294a0bb566c5ec84e25104cfab69
Oleg White <5998684+whitechno@users.noreply.github.com>
2022-01-05 14:57:58 -0800

    Added RandomArray test to StdDraw.

---
+++ algs4j/src/test/java/subutai/algs4j/stddraw/TestMain_05_RandomArraySorted.java
@@ -0,0 +1,25 @@
+package subutai.algs4j.stddraw;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+
+public class TestMain_05_RandomArraySorted {
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(800, 800);
+        StdDraw.setScale(-.1, 1.1);
+
+        int N = 50;
+        double[] a = new double[N];
+        for (int i = 0; i < N; i++) a[i] = StdRandom.uniform();
+        Arrays.sort(a);
+        for (int i = 0; i < N; i++) {
+            double x = 1.0 * i / N;
+            double y = a[i] / 2.0;
+            double rw = 0.48 / N;
+            double rh = a[i] / 2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+}
