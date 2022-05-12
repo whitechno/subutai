df80b21d31471abad4a53cf6c2233070309b0a72
Oleg White <5998684+whitechno@users.noreply.github.com>
2022-05-11 20:58:04 -0700

    Added bash_scripts/README

---
+++ bash_scripts/README.md
@@ -0,0 +1,19 @@
+Experiments with Bash scripting
+===============================
+
+- [Bash wikipedia](https://en.wikipedia.org/wiki/Bash_(Unix_shell))
+
+- [Bash scripting cheatsheet](https://devhints.io/bash)
+
+- [BashGuide](http://mywiki.wooledge.org/BashGuide)
+    - [Tests and Conditionals](
+      http://mywiki.wooledge.org/BashGuide/TestsAndConditionals)
+
+- [Bash Shell Check Whether a Directory is Empty or Not](
+  https://www.cyberciti.biz/faq/linux-unix-shell-check-if-directory-empty/)
+
+- [SC2181: Check exit code directly with e.g. if mycmd;, not indirectly with $?.](
+  https://github.com/koalaman/shellcheck/wiki/SC2181)  
+  To additionally capture output with command substitution:  
+  `if ! output=$(mycommand); then ...`
+
--- bash_scripts/ddd.sh
+++ bash_scripts/ddd.sh
@@ -5,20 +5,41 @@ function error_exit() {
   exit 1
 }
 
+THIS_DIR="${0%/*}"
+echo "Script directory: $THIS_DIR"
+
 TMP_DIR=/tmp
 TMP_EXTRACT=${TMP_DIR}/da_download
 TODAY=$(date '+%Y%m%d')
-echo $TODAY
+echo "$TODAY"
+
 if [ ! -d ${TMP_EXTRACT} ]; then
-  mkdir ${TMP_EXTRACT}
-  if [ "$?" -ne "0" ]; then error_exit "Could not create temporary directory: ${TMP_EXTRACT}"; fi
+  mkdir ${TMP_EXTRACT} ||
+    error_exit "Could not create temporary directory: ${TMP_EXTRACT}"
 fi
+touch "${TMP_EXTRACT}/${TODAY}"
 
 echo "Cleaning up..."
-rm ${TMP_EXTRACT}/*.*
-if [ "$?" -ne "0" ]; then error_exit "Unable to delete contents of ${TMP_EXTRACT}"; fi
-rmdir ${TMP_EXTRACT}
-if [ "$?" -ne "0" ]; then error_exit "Unable to delete directory: ${TMP_EXTRACT}"; fi
-echo "DONE"
 
+# shellcheck disable=SC2016
+: '
+OUT=$(ls -A ${TMP_EXTRACT} 2>/dev/null) # check if dir is non-empty
+echo "$?" # prints exit status of the last command
+echo ">$OUT<" # prints output of the command
+'
+
+# check if dir is non-empty and delete its contents
+# use either 'find'
+# find ${TMP_EXTRACT} -type d -empty -exec echo {} is empty. \;
+# or 'ls -A'
+#if [ "$(ls -A ${TMP_EXTRACT})" ]; then
+if LS_OUT="$(ls -A ${TMP_EXTRACT})" && [ "$LS_OUT" ]; then
+  rm ${TMP_EXTRACT}/* || error_exit "Unable to delete contents of ${TMP_EXTRACT}"
+else
+  echo "${TMP_EXTRACT} is empty"
+fi
+
+rmdir ${TMP_EXTRACT} || error_exit "Unable to delete directory: ${TMP_EXTRACT}"
+
+echo "DONE"
 exit 0
