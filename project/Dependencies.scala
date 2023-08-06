a2a6a124aacf433df61889cf9b76cfcb4c2fa71b
Oleg White <5998684+whitechno@users.noreply.github.com>
2023-08-06 15:29:57 -0700

    Updated Scala 2.12 to 2.12.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val scala210       = "2.10.7"
     val scala211       = "2.11.12"
-    val scala212       = "2.12.17"
+    val scala212       = "2.12.18"
     val scala213       = "2.13.10"
     val scalatest      = "3.2.16"
     val typesafeConfig = "1.4.2"
