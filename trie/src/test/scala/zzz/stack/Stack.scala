90a19be29e856947a901022bdebefbeac77d5a01
Oleg White <5998684+whitechno@users.noreply.github.com>
2024-03-03 15:12:41 -0800

    Minor update to Stack class

--- trie/src/test/scala/zzz/stack/Stack.scala
+++ trie/src/test/scala/zzz/stack/Stack.scala
@@ -1,17 +1,21 @@
 package zzz.stack
 
-class Stack[+A] {
+/** Simple and elegant implementation of stack */
+class Stack[+A] { self => // alias for Stack.this
   import scala.sys.error
   def push[B >: A](elem: B): Stack[B] = new Stack[B] {
-    override def top: B           = elem
-    override def pop: Stack[B]    = Stack.this
-    override def toString: String = elem.toString + " | " + Stack.this.toString()
+    override def top: B        = elem
+    override def pop: Stack[B] = self // Stack.this
+    override def toString: String =
+      elem.toString + " | " + self.toString() // Stack.this.toString()
   }
   def top: A                    = error("no element on stack")
   def pop: Stack[A]             = error("no element on stack")
-  override def toString: String = ""
+  override def toString: String = "*"
 }
 
+// trie/test:runMain zzz.stack.VariancesTest
+// 7 | java.lang.Object@34610473 | hello | *
 private object VariancesTest extends App {
   var s: Stack[Any] = new Stack().push("hello");
   s = s.push(new Object())
